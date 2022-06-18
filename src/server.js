7b8cb1e000cc13bac82a2804f5fdc56ed56768c1
StandinMahesa <ghanesmahesa22@gmail.com>
2022-06-18 13:47:36 +0700

    Making http to https

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const Hapi = require("@hapi/hapi");
 const routes = require("./routes");
-
+const fs = require("fs");
+var tls = {
+  key: fs.readFileSync("/etc/letsencrypt/live/example.com/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/example.com/cert.pem"),
+};
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
@@ -10,6 +14,7 @@ const init = async () => {
         origin: ["*"],
       },
     },
+    tls: tls,
   });
   server.route(routes);
   await server.start();
