562cac3e46078aba9cf7b97b1abea8b35cfb2db0
StandinMahesa <ghanesmahesa22@gmail.com>
2022-06-18 14:09:16 +0700

    Making HTTPS

--- src/server.js
+++ src/server.js
@@ -1,20 +1,15 @@
 const Hapi = require("@hapi/hapi");
 const routes = require("./routes");
-const fs = require("fs");
-var tls = {
-  key: fs.readFileSync("/etc/letsencrypt/live/example.com/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/example.com/cert.pem"),
-};
+
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
+    port: 443,
     host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
     routes: {
       cors: {
         origin: ["*"],
       },
     },
-    tls: tls,
   });
   server.route(routes);
   await server.start();
